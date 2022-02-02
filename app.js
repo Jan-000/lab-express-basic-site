e9100323befdbfd9c4809863f75d4b14942cc511
Jan Gorecki <jasiekgorecki@gmail.com>
2022-02-02 16:10:38 +0100

    usual save, unsolved yet

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const app = express()
 
-app.use(express.static("public"))
+app.use(express.static(__dirname + "/public"))
 
-app.get("/trips", (request, response, next)=>{response.sendFile(__dirname + "/public/views/trips.html")}) 
+app.get("/trips", (request, response, next)=>{response.sendFile(__dirname + "views/trips.html")}) 
 
-app.get("/built", (request, response, next) => response.sendFile(__dirname + "/public/views/built.html"))
+app.get("/built", (request, response, next) => response.sendFile(__dirname + "/views/built.html"))
 
-app.get("/trips", (request, response, next) => response.sendFile(__dirname + "/public/views/trips.html"))
+app.get("/trips", (request, response, next) => response.sendFile(__dirname + "/views/trips.html"))
+
+app.listen(3000, () => console.log("check if listening to port 3000"))
