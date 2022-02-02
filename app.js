b584df988160600ddde286e47caea8654c5e9854
Jan Gorecki <jasiekgorecki@gmail.com>
2022-02-02 22:56:09 +0100

    Lab solved without bonus gallery of pictures

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require("express")
 const app = express()
 
+//app.use(express.static(__dirname + "/public"))
 app.use(express.static(__dirname + "/public"))
 
-app.get("/trips", (request, response, next)=>{response.sendFile(__dirname + "views/trips.html")}) 
+app.get("/trips", (request, response, next)=>{response.sendFile(__dirname + "/views/trips.html")}) 
 
 app.get("/built", (request, response, next) => response.sendFile(__dirname + "/views/built.html"))
 
-app.get("/trips", (request, response, next) => response.sendFile(__dirname + "/views/trips.html"))
+app.get("/discoveries", (request, response, next) => response.sendFile(__dirname + "/views/discoveries.html"))
+
+app.get("/", (request, response, next) =>
+response.sendFile(__dirname + "/views/homepage.html")
+)
 
 app.listen(3000, () => console.log("check if listening to port 3000"))
